faa332749df53124123585b5bb68a849b45abac3
datastohne <stijn.uijen@databricks.com>
2023-03-29 11:08:38 +0200

    test

--- cicd_demo_project/tasks/sample_etl_task.py
+++ cicd_demo_project/tasks/sample_etl_task.py
@@ -26,3 +26,5 @@ def entrypoint():  # pragma: no cover
 # if you're using spark_python_task, you'll need the __main__ block to start the code execution
 if __name__ == '__main__':
     entrypoint()
+
+print('test')
